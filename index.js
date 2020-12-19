4e91cc9ac5a18578c3087741e8ad5479c3e27773
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-19 07:58:31 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(elem => {
+    let arr = elem.split(" ")
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    return arr.join(" ")
+  })
 }
